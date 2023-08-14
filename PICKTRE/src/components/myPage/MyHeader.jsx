8ed3da8aa40a35a8e1fd346ef311c8408f130bed
KimKyuHoi <k546kh@gmail.com>
2023-08-14 16:55:39 +0900

    Feat: 마이페이지 로고 구현 및 SkeletonUi 구현

--- PICKTRE/src/components/myPage/MyHeader.jsx
+++ PICKTRE/src/components/myPage/MyHeader.jsx
@@ -1,12 +1,13 @@
 import { BiBell, BiHomeAlt } from "react-icons/bi";
 import classes from "./MyHeader.module.css"
+import myPageLogo from "../../assets/myPageLogo.png";
 
 const MyHeader = () => {
     return (
         <header className={classes.header}>
             <section className={classes.mainHeader}>
                 <div className={classes.mainHeaderContent}>
-                    <h1>PICKTRE</h1>
+                    <img src={myPageLogo} alt="마이페이지 로고 입니다." className={classes.myPageLogo}/>
                     <div className={classes.headerIcons}>
                         <li><BiHomeAlt size="27" margin-right="19" /></li>
                         <li><BiBell size="27" /></li>
