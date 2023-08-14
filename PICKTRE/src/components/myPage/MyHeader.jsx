99eeebd1e5e5cc581f827b078120d91436d3e574
Quvid <48755156+KimKyuHoi@users.noreply.github.com>
2023-08-14 23:18:14 +0900

    Refactor: Header 코드 분리

--- PICKTRE/src/components/myPage/MyHeader.jsx
+++ PICKTRE/src/components/myPage/MyHeader.jsx
@@ -1,19 +1,10 @@
-import { BiBell, BiHomeAlt } from "react-icons/bi";
 import classes from "./MyHeader.module.css"
-import myPageLogo from "../../assets/myPageLogo.png";
+import Header from "../common/Header";
 
 const MyHeader = () => {
     return (
         <header className={classes.header}>
-            <section className={classes.mainHeader}>
-                <div className={classes.mainHeaderContent}>
-                    <img src={myPageLogo} alt="마이페이지 로고 입니다." className={classes.myPageLogo}/>
-                    <div className={classes.headerIcons}>
-                        <li><BiHomeAlt size="27" margin-right="19" /></li>
-                        <li><BiBell size="27" /></li>
-                    </div>
-                </div>
-            </section>
+            <Header />
             <section className={classes.subHeader}>
                 <article className={classes.pageHeader}>
                     My Page
