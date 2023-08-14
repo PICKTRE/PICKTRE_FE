aa3c6ff00c266b29519ff2b6d2d5d055c0b5436d
junyeokk <cjh4302@gmail.com>
2023-08-15 02:56:36 +0900

    Feat: HomeHeader, HomeFooter

--- PICKTRE/src/components/home/Home.jsx
+++ PICKTRE/src/components/home/Home.jsx
@@ -1,24 +1,17 @@
 import classes from "./Home.module.css";
-import { BiBell } from "react-icons/bi";
+import HomeHeader from "./HomeHeader";
+import HomeReward from "./HomeReward";
+import Footer from "../common/Footer";
 
 const Home = () => {
   return (
     <>
       <div className={classes.container}>
-        <header className={classes.header}>
-          <section className={classes.mainHeader}>
-            <div className={classes.mainHeaderContent}>
-              <h1>PICKTRE</h1>
-              <BiBell size="27" />
-            </div>
-          </section>
-          <section className={classes.subHeader}>
-            <article className={classes.pageHeader}>Home</article>
-          </section>
-        </header>
+        <HomeHeader />
+
         <main className={classes.main}>
           <section className={classes.profileSection}>
-            프로필 섹션입니다.
+            <HomeReward />
           </section>
           <section className={classes.mainPostSection}>
             <div className={classes.mainPost}>
@@ -28,7 +21,7 @@ const Home = () => {
         </main>
 
         {/* footer section */}
-        <footer className={classes.footer}>푸터 섹션입니다.</footer>
+        <Footer />
       </div>
     </>
   );
