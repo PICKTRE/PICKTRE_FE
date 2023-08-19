19b5a0242435196e1dd8708f388a49f381df1b94
junyeokk <cjh4302@gmail.com>
2023-08-20 00:19:13 +0900

    Feat: HomeLevel 색 퍼센티지화

--- PICKTRE/src/components/home/HomeLevel.jsx
+++ PICKTRE/src/components/home/HomeLevel.jsx
@@ -1,12 +1,19 @@
 import classes from "./HomeLevel.module.css";
 
 const HomeLevel = () => {
+  const currentLevelPercentage = 50;
+
   return (
     <>
       <div className={classes.levelSectionWrapper}>
         <span className={classes.levelCharacter}>LEVEL</span>
         <div className={classes.levelSection}>
-          <div className={classes.levelColorSection}></div>
+          <div className={classes.levelColorSection}>
+            <div
+              className={classes.levelGradationColorSection}
+              style={{ width: `${currentLevelPercentage}%` }}
+            ></div>
+          </div>
         </div>
       </div>
     </>
