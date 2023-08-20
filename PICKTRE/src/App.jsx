e192c788c51ff542e9e02e431b0a6e080550e87e
junyeokk <cjh4302@gmail.com>
2023-08-20 20:36:30 +0900

    Feat: Landing page (/)

--- PICKTRE/src/App.jsx
+++ PICKTRE/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import LandingPage from "./components/landingPage/LandingPage";
 import MyPage from "./components/myPage/MyPage";
 import Home from "./components/home/Home";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/home" element={<Home />} />
       </Routes>
