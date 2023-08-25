6a1a5cc012009596e0fd691c4e4a03901be80463
KimKyuHoi <k546kh@gmail.com>
2023-08-25 17:45:01 +0900

    Fix: api 경로 수정

--- PICKTRE/src/components/camera/TrashCameraPlastic.jsx
+++ PICKTRE/src/components/camera/TrashCameraPlastic.jsx
@@ -30,7 +30,8 @@ const Trashcamera = () => {
     6: 'plastic'
   };
   const loadModel = async () => {
-    net = await tf.loadLayersModel("./PICKTRE/dist/ResNet50V2_fine_tuned.json/model.json");
+    const modelUrl = "https://picktre.netlify.app/dist/ResNet50V2_fine_tuned.json/model.json";
+    net = await tf.loadLayersModel(modelUrl);
   };
 
   const run = async () => {
