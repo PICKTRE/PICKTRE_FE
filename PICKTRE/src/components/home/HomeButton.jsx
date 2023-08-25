74bfb239131e32821fcf7fa329d96552935ef41d
junyeokk <cjh4302@gmail.com>
2023-08-25 14:38:16 +0900

    Feat: /home 내 활동 리포트 연결

--- PICKTRE/src/components/home/HomeButton.jsx
+++ PICKTRE/src/components/home/HomeButton.jsx
@@ -1,11 +1,22 @@
 import classes from "./HomeButton.module.css";
+import { useNavigate } from "react-router-dom";
 
 const HomeButton = () => {
+  const navigate = useNavigate();
+  const onClickActivityReport = () => {
+    navigate("/activity-report");
+  };
+
   return (
     <>
       <div className={classes.subButton}>
         <div className={classes.currentReward}>보유 리워드</div>
-        <div className={classes.myActivityReport}>내 활동 리포트</div>
+        <div
+          className={classes.myActivityReport}
+          onClick={onClickActivityReport}
+        >
+          내 활동 리포트
+        </div>
       </div>
     </>
   );
