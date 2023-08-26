133ff051731906230ddfc016f14f62707163bd35
junyeokk <cjh4302@gmail.com>
2023-08-27 00:25:46 +0900

    Refactor: home 화면 이름 static으로 들어가있는 현상 해결

--- PICKTRE/src/components/home/HomeMainPostText.jsx
+++ PICKTRE/src/components/home/HomeMainPostText.jsx
@@ -1,10 +1,24 @@
 import classes from "./HomeMainPostText.module.css";
+import { useState, useEffect } from "react";
+import showProfile from "../../service/showProfile";
 
 const HomeMainPostText = () => {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    showProfile()
+      .then((data) => {
+        setName(data.data.username);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
       <div className={classes.homeMainPostTextSection}>
-        픽트리님! <br />
+        {name}님! <br />
         인기있는 매거진 이야기를 확인해보세요!
       </div>
     </>
